9340d077b55c919fba11676fd3a05b2da6d9a58a
ZemeckisLebowski <zemeckislebowski@gmail.com>
2015-02-03 23:12:18 -0500

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -4,10 +4,12 @@ public class Stock {
 	/* private stock vars.
 	 * String ticker: Ticker of stock. (ex. APPL, YUM, S)
 	 * double price: current price of the stock.
-	 * double momentum: how the stock price will change every tick in percent.*/
+	 * double momentum: how the stock price will change every tick in percent.
+	 * RandomTicker generator: a string of length 1-4 maker.*/
 	private String ticker;
 	private double price;
 	private double momentum;
+	public RandomTicker generator;
 	
 	//the default constructor
 	public Stock(String t, double p, double m){
@@ -17,9 +19,48 @@ public class Stock {
 		momentum = m;
 	}
 	
+	/* a constructor that makes a random ticker name, a price from 1-50,
+	 * and a momentum from 0.01 to 0.02*/
+	public Stock(){
+		ticker = create();
+		price = (Math.random()*50)+1;
+		momentum = (Math.random()*0.01)+0.01;
+	}
+	
+	
 	//apply the momentum change
 	public void changePrice(){
 		price+= (price*momentum);
+	}
+	
+	//make a ticker
+	public String create(){
+		char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		char[] name = new char[4];
+		int nSzz = 0;
+		int rndInt = (int)(Math.random()*4+1);
+		
+		if(rndInt == 1)
+			nSzz = 1;
+		else if(rndInt == 2)
+			nSzz = 2;
+		else if(rndInt == 3)
+			nSzz = 3;
+		else
+			nSzz = 4;
+		
+		for(int i=0; i<nSzz; i++){
+			name[i] = alphabet[(int)(Math.random()*26)];
+		}
+		
+		String returnStr = "";
+		
+		
+		for(int i=0; i<nSzz; i++){
+			returnStr+= name[i];
+		}
+		
+		return returnStr;
 	}
 	
 	//accessors
@@ -43,3 +84,5 @@ public class Stock {
 	}
 	
 }
+
+//stocksgame (c) 2015 ZemeckisLebowski
