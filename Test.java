3102fb0e3f405786f6498a9c0f46af58b0d42325
ZemeckisLebowski <zemeckislebowski@gmail.com>
2015-02-03 23:13:21 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		RandomTicker test = new RandomTicker();
+		System.out.println(test.create());
+		test.newStr();
+		Stock test2 = new Stock();
+		Stock test3 = new Stock();
+		Stock test4 = new Stock();
+		Stock test5 = new Stock();
+		System.out.println(test2);
+		System.out.println(test3);
+		System.out.println(test4);
+		System.out.println(test5);
+
+	}
+
+}
+//This one is in here just so I can have all my code I use online.
+
+//stocksgame (c) 2015 ZemeckisLebowski
